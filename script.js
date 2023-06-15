7331ab34a05004bf9f85966c251a6052472a3bd5
Seonkwan Yoo <seonkwanyoo@gmail.com>
2023-06-15 06:08:41 -0700

    Add Psuedocode

---
+++ script.js
@@ -0,0 +1,10 @@
+// Write a function called playRound with two 
+// parameters (PlayerSelection,computerSelection)
+
+// Create a function called getComputerChoice 
+
+// Randomly generate whole numbers from 1-3. Set each value to either
+// Rock Paper or scissors
+
+
+// create a switch statement to compare the results to each scenario
