a90d154af0691b2c96033e9405adfacb3174ebee
Seonkwan Yoo <seonkwanyoo@gmail.com>
2023-06-15 06:29:25 -0700

    Create function getComputerChoice
    This function will generate whole number between 1-3, and assign each
    number to either rock, paper, or scissor.

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 // Write a function called playRound with two 
-// parameters (PlayerSelection,computerSelection)
-
-// Create a function called getComputerChoice 
-
-// Randomly generate whole numbers from 1-3. Set each value to either
-// Rock Paper or scissors
-
+// parameters (playerSelection,computerSelection)
+function playRound (playerSelection, computerSelection) {
+// Create function computerSelection()
 
 // create a switch statement to compare the results to each scenario
+
+}
+
+
+// Create a function called getComputerChoice 
+function getComputerChoice () {
+    let randomNum = Math.ceil(Math.random() * 3)
+}
+
+// Randomly generate whole numbers from 1-3. Set each value to either
+// Rock Paper or scissor
+
+// return that value, which will be stored as a variable. This variable 
+// will be one of the two paramenters for playRound Function
+
+
